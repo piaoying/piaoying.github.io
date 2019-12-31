a57ebfb19704e7ef9fce64d52adc4d248813d1ca
handa <794363716@qq.com>
2020-01-01 00:02:40 +0800

    Site updated: 2020-01-01 00:02:39

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 31 2019 23:59:19 GMT+0800 (China Standard Time)
+// build time:Wed Jan 01 2020 00:02:37 GMT+0800 (China Standard Time)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
